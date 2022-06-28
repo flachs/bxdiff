b0970065f15957a7e0f988dbd6710ec213414cb3
Brian Flachs <flachs@flachs.us>
2022-06-27 23:17:59 -0500

    use gcc extension for better macros

--- lib/include/common.h
+++ lib/include/common.h
@@ -3,19 +3,20 @@
 #define COMMON 1
 
 /* very useful unix includes */
-#include "stdio.h"
-#include "unistd.h"
-#include "fcntl.h"
-#include "math.h"
-#include "values.h"
-#include "string.h"
-#include "malloc.h"
-#include "sys/types.h"
-#include "signal.h"
-#include "sys/stat.h"
-#include "sys/time.h"
-#include "sys/wait.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+#include <values.h>
+#include <string.h>
+#include <malloc.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 /* some function definitions left out of the system include files */
 #ifdef _HPUX_SOURCE 
@@ -52,15 +53,24 @@ void Zclose(ZFILE *r);
 #define strdiff(a,b) strcmp((a),(b))
 #define strndiff(a,b,n) strncmp((a),(b),(n))
 #define sqr(a) ( (a)*(a) )
-#define abs(a) ( (a) >= 0 ? (a) : -(a) )
-#define sgn(a) ( (a) > 0 ? 1 : ( (a) < 0 ? -1 : 0) )
-#define min(a,b) ( (a) > (b) ? (b) : (a) )
-#define max(a,b) ( (a) > (b) ? (a) : (b) )
-#define downshift(m,n) ( ((n)>0) ? (((m)>>(n))&( ~((unsigned int)1<<(unsigned int)(8*sizeof(m)-1)) )) : (m) )
+#define abs(a) ({ typeof(a)_a=a; (_a >= 0) ? _a : -_a; })
+#define sgn(a) ({ typeof(a)_a=a; (_a > 0)  ?  1 : (_a < 0) ? -1 : 0; })
+#define min(a,b) ({ typeof(a)_a=a; typeof(b)_b=b; (_a > _b) ? _b : _a; })
+#define max(a,b) ({ typeof(a)_a=a; typeof(b)_b=b; (_a > _b) ? _a : _b; })
 #define newitem(i,n) (i *)calloc(n,sizeof(i))
 #define newitem_ni(i,n) (i *)malloc((n)*sizeof(i))
 
+// defense against >> weirdness
+#define downshift(m,n) ({ typeof(m)_m=m,_o=1,_s=8*sizeof(_m)-1;typeof(n)_n=n;\
+     (_n>0) ? ((_m>>_n)& ~(_o<<_s) ) : _m })
 
+// multi-level nested for loops
+// d - number of dimensions
+// s - starting value (array for RVNFL constant for SRVNFL)
+// c - current index array
+// e - ending value or array (while (c<e) ....)
+// i - increment value or array
+// u - code to be looped
 #define RVNFL(d,s,c,e,i,u)                                                   \
   {                                                                          \
   int l=0,f=0;                                                               \
